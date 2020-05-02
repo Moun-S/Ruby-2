c4ffab74ee2b515e47dae2d4f97a0f4f9627da9f
Mounir <sekkalmounir@gmail.com>
2020-05-03 01:53:50 +0200

    update

--- pyramids.rb
+++ pyramids.rb
@@ -1,5 +1,19 @@
+def full_pyramid
 
+puts "Salut, bienvenue dans ma pyramide ! combien d'étage veux-tu ?"
+print =">"
 
+nbr = gets.chomp.to_i
+
+nbr.times do |n|
+
+	print " " * (nbr-n)
+	puts "#" * (2*n+1)
+	
+end
+end
+
+full_pyramid
 
 
 	
